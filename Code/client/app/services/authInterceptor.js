5c3d6bbe70a16b1ddaa9b3350a0740dbc9db8e70
Georgi Prodanov <georgiwe@gmail.com>
2015-02-04 13:43:37 +0200

    Change 401 error interceptor to format the error so the alert service can read it properly.

--- Code/client/app/services/authInterceptor.js
+++ Code/client/app/services/authInterceptor.js
@@ -15,10 +15,14 @@
       responseError: function (error) {
         if (error.status === 401) {
           var message = 'You are not authorized to access this resource';
-          return $q.reject({
+          var err = {
             status: 401,
-            message: message
-          });
+            data: {
+              message: message
+            }
+          };
+
+          return $q.reject(err);
         }
 
         return $q.reject(error);
